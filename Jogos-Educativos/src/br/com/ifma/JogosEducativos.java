3efa18c44961052856d0723d6fd260564db0f662
Pedro <pedrobrito17@gmail.com>
2018-03-31 16:16:57 -0300

    Início do projeto

---
+++ Jogos-Educativos/src/br/com/ifma/JogosEducativos.java
@@ -0,0 +1,13 @@
+package br.com.ifma;
+
+/**
+ *
+ * @author Pedro Brito
+ */
+public class JogosEducativos {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
